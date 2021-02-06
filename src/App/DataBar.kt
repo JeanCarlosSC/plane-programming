fce643f96e026ecd198f6c25def2ae06ac856da8
JeanCarlos911 <jeancarlosodi@gmail.com>
2021-02-06 10:44:15 -0500

    sistema regulador de actividad gaming

--- src/App/DataBar.kt
+++ src/App/DataBar.kt
@@ -12,44 +12,64 @@ import javax.swing.JOptionPane
 object dataBar: SPanel(0, 57, 1290, 32) {
 
     private var stars: Int
+    private var games: Int
+
     private val btStar: SButton
+    private val btGame: SButton
+
     private val lStar: SLabel
+    private val lGame: SLabel
 
     init {
         val file = File("datos.ser")
         if(file.exists() && file.isFile) {
-            stars = ObjectInputStream(FileInputStream("datos.ser")).readObject() as Int
+            val objectInputStream = ObjectInputStream(FileInputStream("datos.ser"))
+            stars = objectInputStream.readObject() as Int
+            games = objectInputStream.readObject() as Int
         }
         else {
             stars = 0
+            games = 0
         }
 
         btStar = SButton(10,5, ImageIcon("resources/star.png"))
         btStar.addMouseListener (object : MouseListener {
             override fun mouseClicked(e: MouseEvent?) {
-                if (e!!.isControlDown) {
-                    stars++
-                }
-                else {
-                    stars--
-                }
-                guardar()
-                actualizar()
+                if (e!!.isControlDown) moreStars()
+                else lessStars()
             }
 
-            override fun mousePressed(e: MouseEvent?) {
-            }
+            override fun mousePressed(e: MouseEvent?) { }
 
-            override fun mouseReleased(e: MouseEvent?) {
-            }
+            override fun mouseReleased(e: MouseEvent?) { }
 
             override fun mouseEntered(e: MouseEvent?) { }
 
             override fun mouseExited(e: MouseEvent?) { }
 
         })
-
         lStar = SLabel(32, 0, 32, 32, "")
+
+        btGame = SButton(70,10, ImageIcon("resources/game.png"))
+        btGame.addMouseListener (object : MouseListener {
+            override fun mouseClicked(e: MouseEvent?) {
+                if (e!!.isControlDown) {
+                    if(stars>0) moreGames()
+                    else JOptionPane.showMessageDialog(null, "No posee suficientes estrellas", "Error", 0)
+                }
+                else lessGames()
+            }
+
+            override fun mousePressed(e: MouseEvent?) { }
+
+            override fun mouseReleased(e: MouseEvent?) { }
+
+            override fun mouseEntered(e: MouseEvent?) { }
+
+            override fun mouseExited(e: MouseEvent?) { }
+
+        })
+        lGame = SLabel(96, 0, 32, 32, "")
 
         border = null
 
@@ -62,11 +82,36 @@ object dataBar: SPanel(0, 57, 1290, 32) {
         actualizar()
     }
 
+    fun lessStars() {
+        stars--
+        guardar()
+        actualizar()
+    }
+
+    fun moreGames() {
+        stars--
+        games += 4
+        guardar()
+        actualizar()
+    }
+
+    fun lessGames() {
+        games--
+        guardar()
+        actualizar()
+    }
+
     private fun actualizar() {
         removeAll()
+
         add(btStar)
         lStar.text = "$stars"
         add(lStar)
+
+        add(btGame)
+        lGame.text = "$games"
+        add(lGame)
+
         repaint()
     }
 
@@ -74,9 +119,10 @@ object dataBar: SPanel(0, 57, 1290, 32) {
         try {
             val salida = ObjectOutputStream(FileOutputStream("datos.ser"))
             salida.writeObject(stars)
+            salida.writeObject(games)
         }
         catch (e: Exception) {
-            JOptionPane.showMessageDialog(null, "No se pudo guardar la cantidad de estrellas", "Error", 0)
+            JOptionPane.showMessageDialog(null, "No se pudo guardar la informacion del data bar", "Error", 0)
         }
     }
 
