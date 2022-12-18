d9f0ccdb1a396b331be50c6342425928a12592f1
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-12-18 10:52:44 -0500

    fixing bugs and making propeller's spin

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(20, 0, 0);
 
     // Start is called before the first frame update
     void Start()
